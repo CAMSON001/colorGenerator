68120fb7ec574c3e2c3072767ddcfb2daa255258
Camara Vamoro <cvamorocamson@gmail.com>
2023-08-19 15:40:33 +0100

    update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let hexInput = document.getElementById("hex-input");
 let inputColor = document.getElementById("input-color")
 let hexValue ="";
+const sliderText = document.getElementById('sliderText');
+const slider = document.getElementById('slider');
+const alteredColor = document.getElementById('altered-color');
 
 
 
-
-function convertHex(hex) {
+function convertHexToRGB(hex) {
     if (isValidHex(hex)) {
         let strippedHex = hex.replace('#', ' ')
 
@@ -21,6 +23,17 @@ function convertHex(hex) {
         return {r,g,b}
     }
     return null
+}
+
+
+
+function convertRGBToHex(r, g, b) {
+      const firstPair = ("0" + r.toString(16)).slice(-2);
+      const secondPair = ("0" + g.toString(16)).slice(-2);
+      const thirdPair = ("0" + b.toString(16)).slice(-2);
+  
+      const hex = "#" + firstPair + secondPair + thirdPair;
+      return hex;
 }
 
 
@@ -40,4 +53,23 @@ hexInput.addEventListener("input", () => {
     
 });
 
-console.log(convertHex("000"));
+const alterColor = (hex, percentage) => {
+    const {r, g, b} = convertHexToRGB(hex);
+    
+    const amount = Math.floor((percentage/100) * 255);
+    
+    const newR = increaseWithin0To255(r,amount);
+    const newG = increaseWithin0To255(g,amount);
+    const newB = increaseWithin0To255(b,amount)
+    console.log({newR, newG, newB});
+    return convertRGBToHex(newR, newG, newB);
+  }
+  
+  const increaseWithin0To255 = (hex, amount) => {
+    return Math.min(255, Math.max(0, hex + amount));
+  }
+
+slider.addEventListener('input', () => {
+    sliderText.textContent = `${slider.value}%`
+})
+
